16d1e7ac486c76be9cb83f69bce0192a8e6d8928
mshood8701 <mosh_shood@hotmail.com>
2025-04-13 13:48:11 +0100

    provider registration was set to true

--- provider.tf
+++ provider.tf
@@ -2,11 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~>3.0.2"
     }
   }
+
+  required_version = ">1.1.0"
 }
 
 provider "azurerm" {
-  features {}
-} 
+  features {
+    
+  }
+
+  skip_provider_registration = true
+}
